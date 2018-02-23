f382b99d4c04e83c4c386ed66145b6941f3636c1
nate johnson <nate@biobright.org>
2018-02-23 17:55:04 -0500

    '

--- build/bundle.js
+++ build/bundle.js
@@ -116,8 +116,7 @@ module.exports =
 	        region: ctx.data.AWS_REGION,
 	        accessKeyId: ctx.data.AWS_ACCESS_KEY_ID,
 	        secretAccessKey: ctx.data.AWS_SECRET_ACCESS_KEY
-	      },
-	      onError: onError
+	      }
 	    });
 
 	    // Start the process.
